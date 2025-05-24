4c7b339f5d1462e33f3c00e6d77659c2cce43388
りなる <78604826+licodeenar@users.noreply.github.com>
2025-05-24 10:00:09 +0900

    順位を排除

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -46,10 +46,10 @@ function drawTable(jasons, elementId, isJson) {
             document.getElementById(elementId).innerHTML = '<span class="note_data_json">' + jasons + '</span>';
         } else {
             obj = JSON.parse(jasons);
-            html = '<table class="note_list"><tr><th>#</th><th>なまえ / ID</th><th><img class="like_icon" src="img/like.svg"></th></tr>'
+            html = '<table class="note_list"><tr><!-- th>#</th --><th>なまえ / ID</th><th><img class="like_icon" src="img/like.svg"></th></tr>'
             for (let i = 0; i < obj.length; i++) {
-                html += '<tr><td class="note_data_id">' +
-                    (i + 1) + '</td>' + 
+                html += '<tr>' +
+                    //'<td class="note_data_id">' + (i + 1) + '</td>' + 
                     '<td>' +
                     '<div class="note_data_container">' +
                     '<div class="note_icon"><a href="' + obj[i].url + '" target="_blank">' + 
